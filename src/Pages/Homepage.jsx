5199966e70e95afd8e327fa44069d2cdb3d4046f
Nayak <anuja.nayak@capgemini.com>
2023-04-22 22:39:52 +0530

    update classname

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -4,8 +4,7 @@ import '../pages/Homepage.css';
 function Homepage() {
   return (
     <div>
-      <div style={{
-        display: 'flex', justifyContent: 'center', background: '#9384d1', height: '35px', width: '100%', paddingTop: '20px'
+      <div style={{display: 'flex', justifyContent: 'center', background: '#9384d1', height: '35px', width: '100%', paddingTop: '20px'
       }}>
         <div>
           <span style={{ color: 'white', fontWeight: 'bold' }}>Quotes App</span>
@@ -15,16 +14,16 @@ function Homepage() {
       <div style={{ color: '#9384d1', marginLeft: '20px', marginTop: '40px' }}>Categories</div>
       <div className='MainContainer'>
 
-        <div style={{ width: '88px', height: '28px', display: 'flex', justifyContent: 'center', background: '#C9A7EB', paddingTop: '3px', borderRadius: '300px' }}>Love</div>
+        <div className='Container'>Love</div>
 
-        <div style={{ width: '88px', height: '28px', display: 'flex', justifyContent: 'center', background: '#C9A7EB', paddingTop: '3px', borderRadius: '300px' }}>Sad</div>
+        <div className='Container'>Sad</div>
 
-        <div style={{ width: '88px', height: '28px', display: 'flex', justifyContent: 'center', background: '#C9A7EB', paddingTop: '3px', borderRadius: '300px' }}>Happy</div>
+        <div className='Container'>Happy</div>
 
-        <div style={{ width: '88px', height: '28px', display: 'flex', justifyContent: 'center', background: '#C9A7EB', paddingTop: '3px', borderRadius: '300px' }}>Motivation</div>
+        <div className='Container'>Motivation</div>
 
-        <div style={{ width: '88px', height: '28px', paddingTop: '3px', display: 'flex', justifyContent: 'center', background: '#C9A7EB', borderRadius: '300px' }}>Success</div>
-      </div>
+        <div className='Container'>Success</div>
+        </div>
     </div>
   )
 }
