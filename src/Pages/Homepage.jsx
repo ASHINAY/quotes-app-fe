9dc80402b483111f04755fae0a31706a31b90ad3
Nayak <anuja.nayak@capgemini.com>
2023-04-28 16:07:19 +0530

    put cursor

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -3,21 +3,19 @@ import '../pages/Homepage.css';
 import { useNavigate, useLocation } from 'react-router-dom'
 
 function Homepage() {
-
   const navigate = useNavigate();
   const onCategoryClick = () => {
 
     navigate("/Quotespage")
   }
-  
   return (
     <div>
-      <div style={{display: 'flex', justifyContent: 'center', background: '#9384d1', height: '35px', width: '100%', paddingTop: '20px'
+      <div style={{
+        display: 'flex', justifyContent: 'center', background: '#9384d1', height: '35px', width: '100%', paddingTop: '20px'
       }}>
         <div>
           <span style={{ color: 'white', fontWeight: 'bold' }}>Quotes App</span>
         </div>
-
       </div>
       <div style={{ color: '#9384d1', marginLeft: '20px', marginTop: '40px' }}>Categories</div>
       <div className='MainContainer'>
@@ -31,7 +29,7 @@ function Homepage() {
         <div className='Container'>Motivation</div>
 
         <div className='Container'>Success</div>
-        </div>
+      </div>
     </div>
   )
 }
