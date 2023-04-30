9a268ae8fb08bad6cb7da01d42c679bddc0d5149
Nayak <anuja.nayak@capgemini.com>
2023-04-30 14:20:07 +0530

    update map method

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../pages/Homepage.css';
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function Homepage() {
   const navigate = useNavigate();
+  const [categoryLabelList, setCategoryLabelList] = useState([]);
+
+  useEffect(() => {
+    setCategoryLabelList(["Love", "Sad", "Happy", "Motivation", "Success"]);
+  }, []);
+
   const onCategoryClick = () => {
 
     navigate("/Quotespage")
@@ -17,18 +23,21 @@ function Homepage() {
           <span style={{ color: 'white', fontWeight: 'bold' }}>Quotes App</span>
         </div>
       </div>
-      <div style={{ color: '#9384d1', marginLeft: '20px', marginTop: '40px' }}>Categories</div>
+      <div style={{ color: '#9384d1', marginLeft: '20px', marginTop: '40px', fontWeight: 'bold' }}>CategoriesLabelList</div>
       <div className='MainContainer'>
+        <div className='Container' onClick={onCategoryClick}>
+          {categoryLabelList.map((categoryLabel, index) => (
+            <div key={index}>{categoryLabel}</div>
+          ))}
 
-        <div className='Container' onClick={onCategoryClick}>Love</div>
+        </div>
+        {/* // <div className='Container'>Sad</div>
 
-        <div className='Container'>Sad</div>
+        // <div className='Container'>Happy</div>
 
-        <div className='Container'>Happy</div>
+        // <div className='Container'>Motivation</div>
 
-        <div className='Container'>Motivation</div>
-
-        <div className='Container'>Success</div>
+        // <div className='Container'>Success</div> */}
       </div>
     </div>
   )
